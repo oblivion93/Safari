6d7491890bfc5a954e2a809c11514dcec463ec18
Mark Aziz <mazi910@aucklanduni.ac.nz>
2015-02-22 18:48:41 +1300

    created layout for home screen

--- app/src/main/java/com/mark/pokemonsafari/HomeActivity.java
+++ app/src/main/java/com/mark/pokemonsafari/HomeActivity.java
@@ -12,6 +12,7 @@ public class HomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
     }
 
 
